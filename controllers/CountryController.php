3e2966a2c977f7ea6ed8fdafbff2685a4295169b
arnofeng <fenglinhappy@gmail.com>
2019-03-10 22:15:03 +0800

    add

--- controllers/CountryController.php
+++ controllers/CountryController.php
@@ -8,7 +8,29 @@
 
 namespace app\controllers;
 
+use yii\web\Controller;
+use yii\data\Pagination;
+use app\models\Country;
 
-class CountryController {
+class CountryController extends Controller
+{
+    public function actionIndex()
+    {
+        $query = Country::find();
 
-} 
+        $pagination = new Pagination([
+            'defaultPageSize' => 5,
+            'totalCount' => $query->count(),
+        ]);
+
+        $countries = $query->orderBy('name')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index', [
+            'countries' => $countries,
+            'pagination' => $pagination,
+        ]);
+    }
+}
